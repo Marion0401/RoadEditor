5a89a3e0424026ae67515fb63e74c7a8cce11de6
64ix <68806646+64ix@users.noreply.github.com>
2021-12-09 14:39:42 +0100

    GridEditor

--- RoadEditor/Assets/Scripts/GridEditor.cs
+++ RoadEditor/Assets/Scripts/GridEditor.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class GridEditor : MonoBehaviour
 {
+    public int height = 100;
+    public int width;
+    public GameObject[,] Roads;
+    public List<GameObject> Forward = new List<GameObject>();
+    public List<GameObject> Intersect = new List<GameObject>();
+    public List<GameObject> Turn = new List<GameObject>();
+    public List<GameObject> T_Road = new List<GameObject>();
+    public GameObject End_Road;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Roads = new GameObject[height, width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Roads[i, j] = Instantiate<GameObject>(Forward[0], new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
