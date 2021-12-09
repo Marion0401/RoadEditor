f9415658e4d1e8bdb093508bc49c1c22460fd0c5
64ix <68806646+64ix@users.noreply.github.com>
2021-12-09 15:11:01 +0100

    Update GridEditor.cs

--- RoadEditor/Assets/Scripts/GridEditor.cs
+++ RoadEditor/Assets/Scripts/GridEditor.cs
@@ -12,19 +12,24 @@ public class GridEditor : MonoBehaviour
     public List<GameObject> Turn = new List<GameObject>();
     public List<GameObject> T_Road = new List<GameObject>();
     public GameObject End_Road;
-
+    public List<Vector3> Directions = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
         Roads = new GameObject[height, width];
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                Roads[i, j] = Instantiate<GameObject>(Forward[0], new Vector3(i, 0, j), Quaternion.identity);
-            }
-        }
+        Directions.Add(Vector3.forward);
+        Directions.Add(Vector3.right);
+        Directions.Add(Vector3.back);
+        Directions.Add(Vector3.left);
+
+        //for (int i = 0; i < height; i++)
+        //{
+        //    for (int j = 0; j < width; j++)
+        //    {
+        //        Roads[i, j] = Instantiate<GameObject>(Forward[0], new Vector3(i, 0, j), Quaternion.identity);
+        //    }
+        //}
     }
 
     // Update is called once per frame
@@ -32,4 +37,38 @@ public class GridEditor : MonoBehaviour
     {
         
     }
+
+    public void AddRoad(Vector3 position)
+    {
+        if (Roads[(int)position.x, (int)position.y] == null)
+        {
+            Roads[(int)position.x, (int)position.y] = Instantiate<GameObject>(Forward[0], position,Quaternion.identity);
+            CheckNeighbors(position);
+        }
+        
+    }
+    public void DeleteRoad(Vector3 pos)
+    {
+        if (Roads[(int)pos.x, (int)pos.y] != null)
+        {
+            Destroy(Roads[(int)pos.x, (int)pos.y]);
+            Roads[(int)pos.x, (int)pos.y] = null;
+        }
+    }
+
+
+
+    public void CheckNeighbors(Vector3 pos)
+    {
+        foreach(var dir in Directions)
+        {
+
+        }
+    }
+
+    public bool IsInGrid(int x, int y)
+    {
+        return (x >= 0 && x < height && y >= 0 && y < width);
+    }
+
 }
